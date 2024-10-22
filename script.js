9e327aef48c4f575049e1486887c5bb38010a878
tsenturion <80580892+tsenturion@users.noreply.github.com>
2024-10-22 15:42:12 +0500

    22.10

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function toggleTask(checkbox) {
     const listItem = checkbox.parentElement;
+    var text_li = listItem.title
     if (checkbox.checked) {
         listItem.classList.add('checked');
-        listItem.innerHTML = `<input type="checkbox" checked onchange="toggleTask(this)"> <del>${listItem.textContent.trim()}</del> <span class="cross">&#10006;</span>`;
+        listItem.innerHTML = `<input type="checkbox" checked onchange="toggleTask(this)"> <del>${text_li}</del> <span class="cross">&#10006;</span>`;
     } else {
         listItem.classList.remove('checked');
-        listItem.innerHTML = `<input type="checkbox" onchange="toggleTask(this)"> ${listItem.textContent.trim()} <span class="cross">&#10006;</span>`;
+        listItem.innerHTML = `<input type="checkbox" onchange="toggleTask(this)"> ${text_li} <span class="cross">&#10006;</span>`;
     }
 }
